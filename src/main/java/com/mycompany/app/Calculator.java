2e48a2118d36317ad0615e7638e7944d854f25c5
sahilmalpotra <sahilmalpotra2007@gmail.com>
2023-08-22 12:04:50 +0530

    v3.1

--- src/main/java/com/mycompany/app/Calculator.java
+++ src/main/java/com/mycompany/app/Calculator.java
@@ -23,6 +23,7 @@ public class Calculator
   public static void main( String[] args )
   {
     System.out.println(Calculator.add(3, 3)); 
-    System.out.println(Calculator.add(4, 6));
+    System.out.println(Calculator.add(2, 6));
+    System.out.println(Calculator.add(3, 8));
   }
 }
